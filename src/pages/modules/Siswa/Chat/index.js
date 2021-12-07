9d28ab1c41df0cefe3caaf89c2260e220119c1a9
muhammadshiva <shivamuhammad07@gmail.com>
2021-12-08 01:36:23 +0700

    update project

--- src/pages/modules/Siswa/Chat/index.js
+++ src/pages/modules/Siswa/Chat/index.js
@@ -6,9 +6,11 @@ import {
   TouchableOpacity,
   FlatList,
 } from 'react-native';
-import {Divider, List, ListItem, Button} from '@ui-kitten/components';
+import {Divider, List, ListItem, Button, Text} from '@ui-kitten/components';
 import firestore from '@react-native-firebase/firestore';
 import Loading from '../../../../components/Loading';
+import moment from 'moment';
+import 'moment/locale/id';
 
 const ChatSiswa = ({navigation}) => {
   const [threads, setThreads] = useState([]);
@@ -24,6 +26,7 @@ const ChatSiswa = ({navigation}) => {
             name: '',
             latestMessage: {
               text: '',
+              createdAt: '',
             },
             ...documentSnapshot.data(),
           };
@@ -42,18 +45,39 @@ const ChatSiswa = ({navigation}) => {
     return <Loading />;
   }
 
+  console.log(moment(threads[0].latestMessage.createdAt).toDate())
+
+
   return (
     <>
       <View style={styles.wrapper}>
         <FlatList
           data={threads}
           renderItem={({item}) => (
-            <ListItem
-              key={item.id}
-              onPress={() => navigation.navigate('Room', {thread: item})}
-              title={item.name}
-              description={item.latestMessage.text}
-            />
+            // <ListItem
+            //   key={item.id}
+            //   onPress={() => navigation.navigate('Room', {thread: item})}
+            //   title={item.name}
+            //   description={item.latestMessage.text}
+            // />
+            <TouchableOpacity style={styles.wrapperCard}  onPress={() => navigation.navigate('Room', {thread: item})}>
+                <View style={styles.wrapper__image}>
+                        <Image
+                            source={{
+                            uri: `https://randomuser.me/api/portraits/lego/${
+                                Math.floor(Math.random() * 9) + 1
+                            }.jpg`,
+                            }}
+                            style={styles.image}
+                        />
+                </View>
+                <View>
+                    <Text style={{fontWeight: 'bold'}}>{item.name}</Text>
+                    {/* <Text>{moment(item.latestMessage.createdAt).toDate().locale('id').format('L')|| ''}</Text> */}
+                    <Text>{item.latestMessage.text || ""}</Text>
+                </View>
+                <View style={styles.border}></View>
+            </TouchableOpacity>
           )}
           keyExtractor={item => item._id}
           style={{marginBottom: 20}}
@@ -66,7 +90,30 @@ const ChatSiswa = ({navigation}) => {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    backgroundColor: '#fff'
   },
+  wrapper__image: {
+    marginLeft: 20
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 20
+  },
+  card: {
+    marginBottom: 10
+  },
+  wrapperCard: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 25,
+      marginTop: 8
+  },
+  border: {
+    borderBottomColor: 'blue',
+    borderBottomWidth: 2
+  }
+
 });
 
 export default ChatSiswa;
